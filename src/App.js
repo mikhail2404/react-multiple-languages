81086322842102366d04c2a97df989cc8ff6c439
Mikhail <mike@drupfan.com>
2022-12-11 17:20:15 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import {useTranslation} from "react-i18next";
+import {Stack, Box, CssBaseline, Typography, MenuItem, TextField, FormControl, InputLabel, Select} from "@mui/material";
+import {useEffect, useState} from "react";
+import 'flag-icon-css/css/flag-icons.css'
+import i18next from "i18next";
+import jsCookie from "js-cookie";
 
+
+const languages = [
+    {
+        code: 'fr',
+        name: 'Français',
+        country_code: 'fr'
+    },
+    {
+        code: 'en',
+        name: 'English',
+        country_code: 'gb'
+    },   {
+        code: 'ar',
+        name: 'العربية',
+        country_code: 'sa',
+        dir: "rtl"
+    },
+
+]
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const currentLanguageCode = jsCookie.get("i18next") || "en"
+    const currentLanguage = languages.find(language => language.code === currentLanguageCode)
+
+    const [language, setLanguage] = useState(currentLanguage)
+    const {t} = useTranslation();
+    const releaseDate = new Date('2021-03-07')
+    const timeDifference = new Date() - releaseDate
+    const number_of_days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+
+    useEffect(() => {
+        document.title = t('app_title')
+        document.body.dir = currentLanguage.dir || "lrt"
+    }, [language, t])
+    const handleChangeLanguage = (event) => {
+        setLanguage(event.target.value)
+        i18next.changeLanguage(event.target.value.code)
+    }
+    return (
+        <>
+            <CssBaseline/>
+            <Stack sx={{alignItems: "center"}} p={"3rem"} direction="row">
+                <Stack sx={{flex: 1}}>
+                    <Typography  variant="h5">{t('welcome_message')}
+                    </Typography>
+                    <Typography variant="body1">{t('days_since_release', {number_of_days, name: "Mikhail"})}</Typography>
+                </Stack>
+                <FormControl sx={{width: "10rem"}}>
+                    <InputLabel id="select-language">Language</InputLabel>
+                    <Select
+                        labelId="select-language"
+                        id="language"
+                        value={language}
+                        label="Age"
+                        onChange={handleChangeLanguage}
+                    >
+                        {languages.map(language => (
+                            <MenuItem  key={language.country_code} value={language}>
+                                <span className={`flag-icon flag-icon-${language.country_code}`}/>
+                                {language.name}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            </Stack>
+        </>
+    );
 }
 
 export default App;
---
+++ src/i18n.js
@@ -0,0 +1,25 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
+// the translations
+// (tip move them in a JSON file and import them,
+// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+i18n
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .use(LanguageDetector)
+    .use(HttpApi)
+    .init({
+        supportedLngs: ['en', 'fr', 'ar'],
+        fallbackLng: 'en',
+        detection: {
+            order: ['path', 'cookie', 'htmlTag', 'localStorage', 'subdomain'],
+            caches: ['cookie']
+        },
+        backend: {
+            loadPath: '/assets/locales/{{lng}}/translation.json',
+        },
+    });
+
+export default i18n;
